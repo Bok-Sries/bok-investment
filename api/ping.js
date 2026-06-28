0bc5b59e931149e893213021a5920aacecf7d318
Bok-Sries <mimibomi0908@gmail.com>
2026-06-28 12:22:39 +0900

    진단: ping을 순수 Node 응답으로 변경

--- api/ping.js
+++ api/ping.js
@@ -1,3 +1,5 @@
-export default function handler(request, response) {
-  response.status(200).json({ ok: true, ts: Date.now() });
+export default function handler(req, res) {
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "application/json; charset=utf-8");
+  res.end(JSON.stringify({ ok: true, ts: Date.now() }));
 }
